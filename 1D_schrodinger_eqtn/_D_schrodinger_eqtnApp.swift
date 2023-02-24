272f79c70b9b9518515419a4ab9c6ac3696dcf04
Andre Guardia <andre.guardiaq@gmail.com>
2023-02-24 17:17:05 -0600

    Initial Commit

---
+++ 1D_schrodinger_eqtn/_D_schrodinger_eqtnApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_schrodinger_eqtnApp.swift
+//  1D_schrodinger_eqtn
+//
+//  Created by IIT Phys 440 on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct _D_schrodinger_eqtnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
